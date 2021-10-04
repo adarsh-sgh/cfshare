bf11981c6e4f9465e2e9f5d71e1ca1e6b9a39761
LAKKI <thakurluckysinghbrh@gmail.com>
2021-10-04 10:13:05 +0530

    eslint and npm initialised

--- import.js
+++ import.js
@@ -1,13 +1,13 @@
-let input=prompt('Paste the problems to import');
-let problems=input.split(',');
-document.body.innerHTML='';
+let input=prompt("Paste the problems to import");
+let problems=input.split(",");
+document.body.innerHTML="";
 problems.forEach(
-id=>buttonFromId(id)
+    id=>buttonFromId(id)
 );
 
 function buttonFromId(problemId){
-let link=`https://codeforces.com/contest/${parseInt(problemId)}/problem/${problemId.replace(/^\d+/,'')}`;
-let htmlTemplate=`<a href='${link}'> Problem ${problemId} </a><br><br>`;
+    let link=`https://codeforces.com/contest/${parseInt(problemId)}/problem/${problemId.replace(/^\d+/,"")}`;
+    let htmlTemplate=`<a href='${link}'> Problem ${problemId} </a><br><br>`;
 
-document.body.innerHTML+=htmlTemplate;
+    document.body.innerHTML+=htmlTemplate;
 }
